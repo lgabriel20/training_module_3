47bc877f6c34bd73879867af959a574c74979e24
Lianne Gabriel <liannegabriel519@yahoo.com>
2025-06-22 16:51:31 -0500

    catch all fallback routes and multiple path segements

--- section-4/app/archive/@archive/[[...filter]]/page.js
+++ section-4/app/archive/@archive/[[...filter]]/page.js
@@ -1,23 +1,52 @@
 import NewsList from "@/components/news-list";
-import { getAvailableNewsYears, getNewsForYear } from "@/lib/news";
+import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from "@/lib/news";
 import Link from "next/link";
 
 export default function FilteredNewsPage({params}) {
   const filter = params.filter;
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  let news;
+  let links = getAvailableNewsYears();
+
+  if(selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if(selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  let newsContent = <p>No news found for the selected period.</p>
+
+  if(news && news.length > 0) {
+    newsContent = <NewsList news={news} />
+  }
+
   console.log(filter);
-  const links = getAvailableNewsYears();
 
   return (
-  <header id="archive-header">
-    <nav>
-      <ul>
-        {links.map((link) => (
-          <li key={link}>
-            <Link href={`/archive/${link}`}>{link}</Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  </header>
-  )
+  <>
+    <header id="archive-header">
+      <nav>
+        <ul>
+          {links.map((link) => {
+            const href = selectedYear
+              ? `/archive/${selectedYear}/${link}`
+              : `/archive/${link}`
+
+            return (
+            <li key={link}>
+              <Link href={href}>{link}</Link>
+            </li>)
+          })}
+        </ul>
+      </nav>
+    </header>
+    {newsContent}
+  </>
+  );
 }
