6d2f12fe657058f699c2cf686614002ee2059078
Lianne Gabriel <liannegabriel519@yahoo.com>
2025-06-22 17:53:36 -0500

    server vs client comp, active paths

--- section-4/components/main-header.js
+++ section-4/components/main-header.js
@@ -1,6 +1,12 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import NavLink from './nav-link';
 
 export default function MainHeader() {
+
+
   return (
     <header id="main-header">
       <div id="logo">
@@ -9,8 +15,11 @@ export default function MainHeader() {
       <nav>
         <ul>
           <li>
-            <Link href="/news">News</Link>
+            <NavLink href="/news" >News</NavLink>
           </li>
+          <li>
+            <NavLink href="/archive" >Archive</NavLink>
+         </li>
         </ul>
       </nav>
     </header>
