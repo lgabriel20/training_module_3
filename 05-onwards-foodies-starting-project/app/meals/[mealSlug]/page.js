0738ff9fb6490456565bf6db3bd6ab70f0563ee9
Lianne Gabriel <lgabriel2@academic.rrc.ca>
2025-06-21 23:15:57 -0500

    added dynamic metadata

--- 05-onwards-foodies-starting-project/app/meals/[mealSlug]/page.js
+++ 05-onwards-foodies-starting-project/app/meals/[mealSlug]/page.js
@@ -3,13 +3,22 @@ import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
 import { notFound } from "next/navigation";
 
-export default function MealDetailsPage({params}) {
+export async function generateMetadata({params}) {
   const meal = getMeal(params.mealSlug);
 
   if(!meal)
- {
-  notFound();
- }
+  {
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary
+  };
+}
+
+export default function MealDetailsPage({params}) {
+  const meal = getMeal(params.mealSlug);
 
   meal.instructions = meal.instructions.replace(/\n/g, '<br />');
 
