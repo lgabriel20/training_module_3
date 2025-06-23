ad39a5daa6d6e5e2ee258e1601965342a993f2fe
Lianne Gabriel <lgabriel2@academic.rrc.ca>
2025-06-23 00:18:58 -0500

    storing server actions in separate files

--- section-6/app/new-post/page.js
+++ section-6/app/new-post/page.js
@@ -1,43 +1,7 @@
 import Postform from "@/components/post-form";
-import { storePost } from "@/lib/posts";
-import { redirect } from "next/navigation";
-
+import { createPost } from "@/actions/posts";
 
 export default function NewPostPage() {
-  async function createPost(prevState, formData) {
-    'use server';
-    const title = formData.get('title');
-    const image = formData.get('image');
-    const content = formData.get('content');
-
-    let errors = [];
-
-    if(!title || title.trim().length === 0){
-      errors.push("Title is required.")
-    }
-
-    if(!content || content.trim().length === 0) {
-      errors.push("Content is required.")
-    }
-
-    if(!image) {
-      errors.push("Image is required.")
-    }
-
-    if(errors.length > 0) {
-      return {errors};
-    }
-
-    await storePost({
-      imageUrl: '',
-      title,
-      content,
-      userId: 1
-    })
-
-    redirect('/feed');
-  }
-
   return <Postform action={createPost}/>
 
 }
