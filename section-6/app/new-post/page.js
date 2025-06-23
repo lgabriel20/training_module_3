67cf086bbbced9aba6aa8340234f7b6e90c84c59
Lianne Gabriel <lgabriel2@academic.rrc.ca>
2025-06-22 23:45:13 -0500

    creating server action

--- section-6/app/new-post/page.js
+++ section-6/app/new-post/page.js
@@ -1,5 +1,6 @@
 export default function NewPostPage() {
-  function createPost() {
+  async function createPost(formData) {
+    'use server';
     const title = formData.get('title');
     const image = formData.get('image');
     const content = formData.get('content');
